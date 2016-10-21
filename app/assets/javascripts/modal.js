84e2afa01d6fa8285a153a323ab3646ac78f040f
Vicente Lucas <vica.escorcio@gmail.com>
2016-10-21 14:15:05 -0300

    remoção do modal.js

--- app/assets/javascripts/modal.js
+++
@@ -1,8 +0,0 @@
-
-$.rails.confirmed = (link) ->
-  linkclass = link.attr 'class'
-  link.removeAttr('data-confirm')
-  link.trigger('click.rails')
-  
-  if linkclass == 'reset-link'
-  	window.location.replace(""+link.context.href+"");
